356ac6c512182a360f129559b5067cae681633d1
Peter Lampard <peter.lampard@pinewood.co.uk>
2023-04-15 21:41:43 +0100

    Additional skeleton required pages

---
+++ Pages/Customer/CustomerManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DeliverySoftware.Pages.Customer
+{
+    public class CustomerManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
