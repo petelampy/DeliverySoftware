61154d4b9e900544b220a09ee673cd0670e0fb87
Peter Lampard <peter.lampard@pinewood.co.uk>
2023-04-16 11:45:28 +0100

    Customer management page

--- Pages/Customer/CustomerManagement.cshtml.cs
+++ Pages/Customer/CustomerManagement.cshtml.cs
@@ -1,12 +1,37 @@
+using DeliverySoftware.Business.Fleet;
+using DeliverySoftware.Business.Users;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace DeliverySoftware.Pages.Customer
 {
     public class CustomerManagementModel : PageModel
     {
-        public void OnGet()
+        private const string PERMISSION_DENIED_PAGE_PATH = "../PermissionDenied";
+        private readonly IUserController __UserController;
+
+        public CustomerManagementModel ()
         {
+            __UserController = new UserController();
         }
+        public IActionResult OnGet ()
+        {
+            string _CurrentUserID = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            DeliveryUser _CurrentUser = __UserController.Get(new Guid(_CurrentUserID));
+
+
+            if (_CurrentUser.UserType == UserType.Employee)
+            {
+                Customers = __UserController.GetAllCustomers();
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage(PERMISSION_DENIED_PAGE_PATH);
+            }
+        }
+
+        public List<DeliveryUser> Customers { get; set; }
     }
 }
